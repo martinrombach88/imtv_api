e900fd56dd3010a9fd800d6cc140e7070f4e6010
Martin Rombach <martinrombach88@gmail.com>
2022-08-30 21:10:27 +0900

    deployment

--- util/database.js
+++ util/database.js
@@ -1,7 +1,7 @@
 const mysql = require("mysql2");
 
 const pool = mysql.createPool({
-  connectionLimit: 6,
+  connectionLimit: 2,
   host: "us-cdbr-east-06.cleardb.net",
   user: "bd95792bb84300",
   database: "heroku_caa56290b6f5a69",
