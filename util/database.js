092df73f2bbe5dbf34e83433b714d6987c83cfe0
Martin Rombach <martinrombach88@gmail.com>
2022-08-30 17:50:01 +0900

    heroku deployment:

--- util/database.js
+++ util/database.js
@@ -2,9 +2,9 @@ const mysql = require("mysql2");
 
 const pool = mysql.createPool({
   host: "us-cdbr-east-06.cleardb.net",
-  user: "b0328f4f47da09",
-  database: "heroku_dcff2cd9babd987",
-  password: "ce04deac",
+  user: "bd95792bb84300",
+  database: "heroku_caa56290b6f5a69",
+  password: "7395f4a9",
 });
 
 module.exports = pool.promise();
