ea4717a5a7f78e992d63e7a615cbf6d59bf39ccc
Martin Rombach <martinrombach88@gmail.com>
2022-08-30 20:31:39 +0900

    heroku connection

--- util/database.js
+++ util/database.js
@@ -1,10 +1,11 @@
 const mysql = require("mysql2");
 
-const db = mysql.createConnection({
+const pool = mysql.createPool({
+  connectionLimit: 6,
   host: "us-cdbr-east-06.cleardb.net",
   user: "bd95792bb84300",
   database: "heroku_caa56290b6f5a69",
   password: "7395f4a9",
 });
 
-module.exports = db.promise();
+module.exports = pool.promise();
