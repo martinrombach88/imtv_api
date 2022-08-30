e1de44c0a44d19427a598848b68efea51928312a
Martin Rombach <martinrombach88@gmail.com>
2022-08-30 17:38:56 +0900

    heroku deployment:

--- util/database.js
+++ util/database.js
@@ -1,6 +1,6 @@
 const mysql = require("mysql2");
 
-const pool = mysql.createConnection({
+const pool = mysql.createPool({
   host: "us-cdbr-east-06.cleardb.net",
   user: "b0328f4f47da09",
   database: "heroku_dcff2cd9babd987",
